711c83491c0b68d5e9761b4154d287c62c8fcd7c
seclark <susanclark19@gmail.com>
2017-12-18 17:05:00 -0500

    test

--- hypercuber.py
+++ hypercuber.py
@@ -20,13 +20,23 @@ class HyperCube():
         self.cubehalfy = np.int(np.floor(ny/2.0))
         
         self.hypercube = np.zeros((ny, nx, nvel, ntheta), np.float_)
+        self.weights_hypercube = np.zeros((ny, nx, nvel, ntheta), np.float_)
         
     def load_2d_data(self, datatype="nhi"):
         
         if datatype == "nhi":
-            self.twoddata_fn = "/disks/jansky/a/users/goldston/zheng/151019_NHImaps_SRcorr/data/GNHImaps_SRCORR_final/NHImaps/GALFA-HI_NHISRCORR_VLSR-90+90kms.fits"
+            #self.twoddata_fn = "/disks/jansky/a/users/goldston/zheng/151019_NHImaps_SRcorr/data/GNHImaps_SRCORR_final/NHImaps/GALFA-HI_NHISRCORR_VLSR-90+90kms.fits"
+            self.twoddata_fn = "/Users/susanclark/Dropbox/NHImaps/GALFA-HI_NHImap_SRcorr_VLSR-090+0090kms.fits"
         
         self.twoddata = fits.getdata(self.twoddata_fn)
+        
+    
+    def load_mask(self, mask=None):
+        """
+        load generic mask or set data == NaN to 0 in mask
+        """
+        if mask is not None:
+            self.mask = np.isfinite(self.twoddata)
         
     def tabulate_per_vel_theta(self, vel_i=0, theta_i=0, verbose=False):
         """
@@ -40,9 +50,6 @@ class HyperCube():
         velthet_fn = velthet_root + "GALFA_HI_W_"+velrangestr+"_newhdr_SRcorr_w75_s15_t70_theta_"+str(theta_i)+".fits"
         velthet = fits.getdata(velthet_fn)
         
-        velthet = np.zeros(velthet.shape)
-        velthet[5, 200] = 1.
-        
         self.maxny, self.maxnx = velthet.shape
         
         nonzeroyx = np.nonzero(velthet)
@@ -52,62 +59,78 @@ class HyperCube():
         if verbose:
             print("There are {} nonzero indices in this map".format(len(nonzerox)))
         
+        # Step through nonzero pixels
         for (_y, _x) in zip(nonzeroy, nonzerox):
+            
+            # the RHT value of the center pixel is the weight
             centerval = velthet[_y, _x]
             
-            print(_x, self.cubehalfx)
             self.startx = np.int(max(0, _x - self.cubehalfx))
             self.stopx = np.int(min(self.maxnx, _x + self.cubehalfx + 1))
             self.starty = np.int(max(0, _y - self.cubehalfy))
             self.stopy = np.int(min(self.maxny, _y + self.cubehalfy + 1))
             
             self.RAedgeflag = False
-            # deal with edges
-            if self.startx == 0:
+            
+            # LH side RA wrap case
+            if (_x - self.cubehalfx) < 0:
                 # deal with RA wrap
                 self.RAedgeflag = True
                 self.startx1 = 0
-                self.stopx1 = _x + self.cubehalfx # maybe +1
-                self.startx2 = self.maxnx - (_x - self.cubehalfx)
+                self.stopx1 = _x + self.cubehalfx + 1 # maybe +1
+                self.startx2 = self.maxnx - (self.cubehalfx - _x)#self.maxnx - _x - self.cubehalfx
                 self.stopx2 = self.maxnx
                 
                 # also, partial placement into hypercube
                 self.smallstartx1 = 0
                 self.smallstartx2 = self.cubehalfx - _x
-                
-            if self.stopx == self.maxnx:
+              
+            # RH side RA wrap case  
+            if (_x + self.cubehalfx + 1) > self.maxnx:
                 self.RAedgeflag = True
                 self.startx1 = 0
-                self.stopx1 = self.cubehalfx - self.maxnx + _x
+                self.stopx1 = self.cubehalfx + 1 - self.maxnx + _x
                 self.startx2 = _x - self.cubehalfx
                 self.stopx2 = self.maxnx
                 
                 # partial placement into hypercube
-                self.smallstartx1 = self.maxnx - (_x - self.cubehalfx)
-                self.smallstartx2 = 0
+                self.smallstartx2 = self.maxnx - (_x - self.cubehalfx)
+                self.smallstartx1 = 0
             
             
             # Are you sitting on the edge in DEC?
             if (_y - self.cubehalfy) < 0:
-                self.smallstarty = self.cubehalfy - _y
+                self.smallstarty = self.cubehalfy - _y 
+                if verbose:
+                    print("DEC < smallest DEC")
             else:
                 self.smallstarty = 0
             
             if (_y + self.cubehalfy) > self.maxny:
-                self.smallstopy = self.cubehalfy*2 + 1 - (_y + self.cubehalfy - self.maxny)
+                self.smallstopy = self.cubehalfy*2 - (_y + self.cubehalfy - self.maxny)
+                if verbose:
+                    print("DEC > largest DEC")
             else:
                 self.smallstopy = self.cubehalfy*2 + 1
             
-            print(self.smallstarty, self.smallstopy)
-            
+            # set into cubes, dealing w/ RA wrap
             if self.RAedgeflag:
-                self.hypercube[self.smallstarty:self.smallstopy, self.smallstartx1:self.smallstartx2, vel_i, theta_i] = centerval*self.twoddata[self.starty:self.stopy, self.startx1:self.stopx]
-                self.hypercube[self.smallstarty:self.smallstopy, self.smallstartx2:, vel_i, theta_i] = centerval*self.twoddata[self.starty:self.stopy, self.startx2:self.maxnx]
+                self.hypercube[self.smallstarty:self.smallstopy, self.smallstartx2:, vel_i, theta_i] += centerval*self.twoddata[self.starty:self.stopy, self.startx1:self.stopx1]
+                self.hypercube[self.smallstarty:self.smallstopy, self.smallstartx1:self.smallstartx2, vel_i, theta_i] += centerval*self.twoddata[self.starty:self.stopy, self.startx2:self.maxnx]
+                
+                self.weights_hypercube[self.smallstarty:self.smallstopy, self.smallstartx1:self.smallstartx2, vel_i, theta_i] += centerval
             
+            # otherwise, proceed
             else:
-                self.hypercube[self.smallstarty:self.smallstopy, :, vel_i, theta_i] = centerval*self.twoddata[self.starty:self.stopy, self.startx:self.stopx]
-            
+                self.hypercube[self.smallstarty:self.smallstopy, :, vel_i, theta_i] += centerval*self.twoddata[self.starty:self.stopy, self.startx:self.stopx]
+                
+                self.weights_hypercube[self.smallstarty:self.smallstopy, :, vel_i, theta_i] += centerval
+                
+                
 #test
 hcube = HyperCube()
 hcube.load_2d_data(datatype="nhi")
-hcube.tabulate_per_vel_theta(vel_i=0, theta_i=0, verbose=True)
+hcube.tabulate_per_vel_theta(vel_i=0, theta_i=0, verbose=False)
+
+np.save("hypercube_test.npy", hcube.hypercube)
+
