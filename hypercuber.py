d1ad8e96e1b4d8215d6c5c4ff0bfb6c960b17c90
seclark <susanclark19@gmail.com>
2017-12-18 15:21:36 -0500

    cube slicing

--- hypercuber.py
+++ hypercuber.py
@@ -19,7 +19,7 @@ class HyperCube():
         self.cubehalfx = np.int(np.floor(nx/2.0))
         self.cubehalfy = np.int(np.floor(ny/2.0))
         
-        self.hypercube = np.zeros((nx, ny, nvel, ntheta), np.float_)
+        self.hypercube = np.zeros((ny, nx, nvel, ntheta), np.float_)
         
     def load_2d_data(self, datatype="nhi"):
         
@@ -58,12 +58,40 @@ class HyperCube():
             starty = np.int(max(0, _y - self.cubehalfy))
             stopy = np.int(min(maxny, _y + self.cubehalfy))
             
+            # deal with edges
+            if startx == 0:
+                # deal with RA wrap
+                self.edgeflag = True
+                startx1 = 0
+                stopx1 = _x + self.cubehalfx # maybe +1
+                startx2 = maxnx - (_x - self.cubehalfx)
+                stopx2 = maxnx
+                
+                # also, partial placement into hypercube
+                smallstartx1 = 0
+                smallstartx2 = self.cubehalfx - _x
+                
+            if stopx == maxnx:
+                self.edgeflag = True
+                startx1 = 0
+                stopx1 = self.cubehalfx - maxnx + _x
+                startx2 = _x - self.cubehalfx
+                stopx2 = maxnx
+                
+                # partial placement into hypercube
+                smallstartx1 = maxnx - (_x - self.cubehalfx)
+                smallstartx2 = 0
+            
             print(startx, stopy, starty, stopy)
             
             print("centerval is ", centerval)
             
+            if self.edgeflag:
+                self.hypercube[starty:stopy, smallstartx1:stopx, vel_i, theta_i] = centerval*self.twoddata[starty:stopy, startx1:stopx]
+                self.hypercube[starty:stopy, startx1:stopx, vel_i, theta_i] = centerval*self.twoddata[starty:stopy, startx2:maxnx]
             
-            self.hypercube[starty:stopy, startx:stopx, vel_i, theta_i] = centerval*self.twoddata[starty:stopy, startx:stopx]
+            else:
+                self.hypercube[:, :, vel_i, theta_i] = centerval*self.twoddata[starty:stopy, startx:stopx]
             
 #test
 hcube = HyperCube()
