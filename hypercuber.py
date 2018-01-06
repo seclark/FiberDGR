a823b3a93e9e503d777a990975032da35aba72f8
seclark <susanclark19@gmail.com>
2018-01-06 02:45:56 -0500

    save hypercube slices, 16, 80 to 165

--- hypercuber.py
+++ hypercuber.py
@@ -170,7 +170,7 @@ hcube = HyperCube(singlecube=False)
 hcube.load_nhi_rad_857(local=False)
 
 for _v in [16]: # of 21
-    for _thet in np.arange(80): # of 165
+    for _thet in np.arange(80, 165): # of 165
         time0 = time.time()
         hcube.tabulate_per_vel_theta(vel_i=_v, theta_i=_thet, verbose=False)
         time1 = time.time()
