8f0369928263753d59eb625174bbedaac70b0d92
seclark <susanclark19@gmail.com>
2018-01-06 01:24:16 -0500

    save hypercube slices, 2, 0 to 80

--- hypercuber.py
+++ hypercuber.py
@@ -168,7 +168,7 @@ class HyperCube():
 hcube = HyperCube(singlecube=False)
 hcube.load_nhi_rad_857(local=False)
 
-for _v in [1]: # of 21
+for _v in [2]: # of 21
     for _thet in np.arange(80): # of 165
         hcube.tabulate_per_vel_theta(vel_i=_v, theta_i=_thet, verbose=False)
         
