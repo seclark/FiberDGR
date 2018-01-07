2b4b13d102180a48f4096bd8241c5a2fb96d21e5
seclark <susanclark19@gmail.com>
2018-01-06 23:57:49 -0500

    purcell runs very slow

--- hypercuber.py
+++ hypercuber.py
@@ -170,6 +170,7 @@ hcube = HyperCube(singlecube=False)
 hcube.load_nhi_rad_857(local=False)
 
 for _v in [6]: # of 21
+    print("running velocity {}".format(_v))
     for _thet in np.arange(20, 80): # of 165
         time0 = time.time()
         hcube.tabulate_per_vel_theta(vel_i=_v, theta_i=_thet, verbose=False)
