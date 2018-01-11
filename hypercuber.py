55f12a867df68a74def68638cee2fcb050580656
seclark <susanclark19@gmail.com>
2018-01-11 11:11:06 -0500

    vel 14 20 to 40

--- hypercuber.py
+++ hypercuber.py
@@ -201,7 +201,7 @@ hcube.load_nhi_rad_857(local=False)
 
 for _v in [14]: # of 21
     print("running velocity {}".format(_v))
-    for _thet in np.arange(0, 20): # of 165
+    for _thet in np.arange(20, 40): # of 165
         time0 = time.time()
         hcube.tabulate_per_vel_theta(vel_i=_v, theta_i=_thet, verbose=False)
         time1 = time.time()
