1ffa7fed20ac7571d8ea56fb1ffa1b8c24546458
seclark <susanclark19@gmail.com>
2018-03-07 23:43:28 -0500

    vel 13-16, b 0 to 10

--- hypercuber.py
+++ hypercuber.py
@@ -267,7 +267,7 @@ hcube.load_nhi_rad_857(local=False)
 bstart=0
 bstop=10
 
-for _v in [9, 10, 11, 12]: # of 21
+for _v in [13, 14, 15, 16]: # of 21
     print("running velocity {}".format(_v))
     for _thet in np.arange(165): # of 165
     
