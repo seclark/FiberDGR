b19d4ff38e7b5d8d624be0abeb71a3e7d2b8c72b
seclark <susanclark19@gmail.com>
2018-03-12 19:24:02 -0400

    biastest, vel 18-20, all b

--- hypercuber.py
+++ hypercuber.py
@@ -282,7 +282,7 @@ zstart=0.89
 zstop=0.91
 biastest=True
 
-for _v in [15,16,17]: # of 21
+for _v in [18,19,20]: # of 21
     print("running velocity {}".format(_v))
     for _thet in np.arange(165): # of 165
     
