9bdbabcd79a588641c71ea026f1856d96b3360e5
seclark <susanclark19@gmail.com>
2018-04-04 00:31:49 -0400

    biastest 0.87 to 0.93, v 3-5

--- hypercuber.py
+++ hypercuber.py
@@ -312,7 +312,7 @@ if biastest is True:
     zstart=0.87#0.89
     zstop=0.93#0.91
 
-for _v in [0, 1, 2]: # of 21
+for _v in [3, 4, 5]: # of 21
     print("running velocity {}".format(_v))
     for _thet in np.arange(165): # of 165
     
