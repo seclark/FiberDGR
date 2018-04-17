f18192dab16af3e106fa01ff399dcb986d22aac1
seclark <susanclark19@gmail.com>
2018-04-17 00:26:45 -0400

    b 30-90, bias test, vel 13-15

--- hypercuber.py
+++ hypercuber.py
@@ -375,7 +375,7 @@ if biastest is True:
     zstart=0.89
     zstop=0.91
 
-for _v in [10, 11, 12]: # of 21
+for _v in [13, 14, 15]: # of 21
     print("running velocity {}".format(_v))
     for _thet in np.arange(0, 165): # of 165
     
