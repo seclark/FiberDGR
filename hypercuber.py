c759a71b09443230c14e50ecafeea2dca411739a
seclark <susanclark19@gmail.com>
2018-05-25 18:23:03 -0400

    starting hypercuber for zcut 0.7-0.77

--- hypercuber.py
+++ hypercuber.py
@@ -372,7 +372,7 @@ bstop=90#bstop=70 #bstop=90#10
 
 if biastest is True:
     zstart=0.70
-    zstop=0.76
+    zstop=0.77
 
 for _v in np.arange(21): # of 21
     print("running velocity {}".format(_v))
