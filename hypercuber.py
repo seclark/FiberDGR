2b0de1dcbd282daa37fa350202ff47671760302d
seclark <susanclark19@gmail.com>
2018-06-07 17:48:37 -0400

    check minicubes 0.7 to 0.73

--- hypercuber.py
+++ hypercuber.py
@@ -443,7 +443,8 @@ np.save("hcubes/hypercube_weights_bstart_{}_bstop_{}.npy".format(bstart, bstop),
 
 bstart=30
 bstop=90
-zstart=0.73
+zstart=0.70
+zstop=0.73
 minicube=True
 
 hcube = HyperCube(singlecube=False)
