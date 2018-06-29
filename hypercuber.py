dec97740ea0ba861b31f5f5c8db65925ba3acf50
seclark <susanclark19@gmail.com>
2018-06-29 12:00:22 -0400

    minicube 0.8-0.81, vel9

--- hypercuber.py
+++ hypercuber.py
@@ -405,9 +405,9 @@ else:
 
 if biastest is True:
     zstart=0.80
-    zstop=0.82#0.76#0.85
+    zstop=0.81#0.76#0.85
     
-for _v in [10]: # of 21
+for _v in [9]: # of 21
     print("running velocity {}".format(_v))
     for _thet in np.arange(165): # of 165
     
