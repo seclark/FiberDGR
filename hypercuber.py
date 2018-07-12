308308ad10d83d756e3fa799f4d15abf6b410d56
seclark <susanclark19@gmail.com>
2018-07-12 14:59:39 -0400

    centerweighted biastest, 0.76-0.79 vel 9

--- hypercuber.py
+++ hypercuber.py
@@ -417,7 +417,7 @@ if biastest is True:
     zstart=0.76
     zstop=0.79#0.76#0.85
     
-for _v in [10]: # of 21
+for _v in [9]: # of 21
     print("running velocity {}".format(_v))
     for _thet in np.arange(165): # of 165
     
