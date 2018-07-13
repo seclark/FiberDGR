a70d2399460010c3e3a91a70607eea821b0efb78
seclark <susanclark19@gmail.com>
2018-07-13 16:36:49 -0400

    centerweighted biastest, 0.88-0.91

--- hypercuber.py
+++ hypercuber.py
@@ -414,8 +414,8 @@ if biastest:
         centervalstr = ""
 
 if biastest is True:
-    zstart=0.85
-    zstop=0.88#0.76#0.85
+    zstart=0.88
+    zstop=0.91#0.76#0.85
     
 for _v in [9, 10, 11]: # of 21
     print("running velocity {}".format(_v))
