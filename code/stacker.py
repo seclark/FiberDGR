18f24614ff438fe7a9228d2d16cbb529ecc1c7e1
seclark <susanclark19@gmail.com>
2018-11-21 03:05:49 -0500

    new rot code midwidth bootstrap

--- code/stacker.py
+++ code/stacker.py
@@ -514,7 +514,7 @@ def stack_on_USM(bsnum=0, bootstrapchunks=False):
         
     cubelen = 181
 
-    Narrow=True
+    Narrow=False
     nulltest=False
     randomorient=True
         
@@ -523,9 +523,9 @@ def stack_on_USM(bsnum=0, bootstrapchunks=False):
     datatypelist = ["NHI90", "P3_857"]
     #vels=["1020", "1021", "1022", "1023", "1024", "1025", "1026", "1027", "1028"]
     #vels=["1021", "1022", "1023", "1024", "1025", "1026", "1027"]
-    #vels=["1022", "1023", "1024", "1025", "1026"]
+    vels=["1022", "1023", "1024", "1025", "1026"]
     #vels=["1023", "1024", "1025"]
-    vels=["1024"]
+    #vels=["1024"]
     #vels="NHI"
     
     time0 = time.time()
@@ -594,7 +594,7 @@ def assemble_hypercube():
 if __name__ == "__main__":
     #stack_on_RHT()
     
-    nchunks=40
+    nchunks=20
     for _bsnum in np.arange(nchunks):
         stack_on_USM(bsnum=_bsnum, bootstrapchunks=nchunks)
     #stack_on_USM()
