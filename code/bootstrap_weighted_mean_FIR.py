c03b2c10c1362be802f2a928ce40147d6962518d
seclark <susanclark19@gmail.com>
2018-12-12 15:23:42 -0500

    vels9

--- code/bootstrap_weighted_mean_FIR.py
+++ code/bootstrap_weighted_mean_FIR.py
@@ -297,7 +297,7 @@ if __name__ == "__main__":
         _mtest = make_mask_2d(bstart=30, bstop=90, PS=True, bootstrapchunks=Nblocks, bsnum=_i)
         allblocks.append(_mtest)
         
-    vels=["1021", "1022", "1023", "1024", "1025", "1026", "1027"]
+    vels=["1020", "1021", "1022", "1023", "1024", "1025", "1026", "1027", "1028"]
     umask = get_USM_slice(vels, fwhm=30, zeroed=True, Narrow=False, reverse=False, writemap=False)
 
     allP857blocks = []
